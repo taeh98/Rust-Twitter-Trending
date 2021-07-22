e9010f56c96d9eb321a282963278ca147ae4df7e
Tom <thomasaeh1@gmail.com>
2021-07-22 20:43:23 +0100

    got process_dataframe() in get_tweets.rs to work in parallel

--- src/get_tweets.rs
+++ src/get_tweets.rs
@@ -7,12 +7,11 @@ use polars::series::Series;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 fn process_dataframe(df: DataFrame) -> Vec<String> {
-    println!("process_dataframe()");
     let column: &Series = df.column("text").unwrap();
 
     let res: Mutex<Vec<String>> = Mutex::new(Vec::new());
 
-    let indices: Vec<usize> = vec![0, 1, 2];
+    let indices: Vec<usize> = (0..column.len()).collect();
 
     indices.into_par_iter()
         .map(|val: usize| column.str_value(val).to_string())
@@ -21,20 +20,16 @@ fn process_dataframe(df: DataFrame) -> Vec<String> {
             v.push(str);
         });
 
-    println!("process_dataframe(), res = {:#?}", res);
-
     res.into_inner().unwrap()
 }
 
 pub fn get_tweets() -> Option<Vec<String>> {
-    println!("get_tweets()");
     return match CsvReader::from_path("data/covid19_tweets.csv") {
         Ok(file_data) => {
             match file_data.infer_schema(None)
                 .has_header(true)
                 .finish() {
                 Ok(df) => {
-                    println!("get_tweets(), going to process_dataframe()");
                     Some(process_dataframe(df))
                 }
                 _ => {
