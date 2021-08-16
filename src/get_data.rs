b5a747c1f777789a11903691ee666544f1090345
Tom Hardern <thomasaeh1@gmail.com>
2021-08-16 19:27:17 +0100

    added time taken to output

--- src/get_data.rs
+++ src/get_data.rs
@@ -1,11 +1,12 @@
 /**
 
 1. go to https://doi.org/10.5281/zenodo.3723939 for latest version of https://github.com/thepanacealab/covid19_twitter
-2. find full_dataset_clean.tsv.gz element
-3. get download link and its md5 checksum digest
-4. download the dataset
-5. verify integrity against the md5 digest
-6. extract the .tsv.gz file to a .tsv file
-7. delete the .tsv.gz file
+2. find full_dataset_clean.tsv.gz element and current date or version
+3. if latest version already downloaded then exit
+4. get download link and its md5 checksum digest
+5. download the dataset
+6. verify integrity against the md5 digest
+7. extract the .tsv.gz file to a .tsv file
+8. delete the .tsv.gz file
 
  */
