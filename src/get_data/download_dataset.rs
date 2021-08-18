b0e7ed10040a110e8228550085758032ad916278
Tom <thomasaeh1@gmail.com>
2021-08-18 21:14:18 +0100

    added md5 hash checking to download_dataset.rs

--- src/get_data/download_dataset.rs
+++ src/get_data/download_dataset.rs
@@ -1,11 +1,43 @@
+use std::fs::read_to_string;
+
+use md5::compute;
+
 pub fn download_dataset(
     current_dataset_file_link: String,
     current_dataset_file_md5_digest: String,
     extracted_data_file_path: String,
     compressed_data_file_path: String,
 ) {
-    //TODO: download latest file with progress bar (like wget)
-    //TODO: once downloaded, verify downloaded file against md5 digest
+    download_compressed_dataset_file(&current_dataset_file_link, &extracted_data_file_path);
+    if !verify_compressed_dataset_file(&current_dataset_file_md5_digest, &extracted_data_file_path)
+    {
+        panic!("The compressed dataset file failed md5 checksum verification.");
+    }
     //TODO: once verified, extract downloaded .tsv.gz file to a .tsv file
     //TODO: once extracted, delete the .tsv.gz file
+}
+
+fn verify_compressed_dataset_file(
+    current_dataset_file_md5_digest: &String,
+    extracted_data_file_path: &String,
+) -> bool {
+    match file_to_bytes(extracted_data_file_path) {
+        Some(file_bytes) => compute(file_bytes).eq(current_dataset_file_md5_digest.as_bytes()),
+        _ => false,
+    }
+}
+
+fn file_to_bytes(filepath: &String) -> Option<&[u8]> {
+    match read_to_string(filepath).ok() {
+        Some(contents) => Some(contents.as_bytes()),
+        _ => None,
+    }
+}
+
+fn download_compressed_dataset_file(
+    current_dataset_file_link: &String,
+    extracted_data_file_path: &String,
+) {
+
+    //TODO: download latest file with progress bar (like wget)
 }
