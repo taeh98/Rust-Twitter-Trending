e8d5147c0786d07abfc15987b4cf1d72e717a413
Tom Hardern <thomasaeh1@gmail.com>
2021-08-20 14:54:51 +0100

    first attempt at allowing process_tweets.rs to run both in serial and parallel

--- src/process_tweets.rs
+++ src/process_tweets.rs
@@ -11,16 +11,30 @@ use rayon::prelude::*;
 //TODO: try to debug errors that came from using reduce() rather than reduce_with() throughout project
 //TODO: integrate the use of "if let" throughout the project
 
-pub fn process_tweets(tweets: Vec<String>) -> PriorityQueue<String, i128> {
-    processed_tweets_to_priority_queue(
-        tweets
-            .par_iter()
-            .map(|tweet: &String| process_tweet(tweet))
-            .reduce_with(|a: DashMap<String, i128>, b: DashMap<String, i128>| {
-                combine_processed_tweets(&a, &b)
-            })
-            .unwrap(),
-    )
+pub fn process_tweets(tweets: &Vec<String>, parallel: bool) -> PriorityQueue<String, i128> {
+    if parallel {
+        processed_tweets_to_priority_queue(
+            tweets
+                .par_iter()
+                .map(|tweet: &String| process_tweet(tweet))
+                .reduce_with(|a: DashMap<String, i128>, b: DashMap<String, i128>| {
+                    combine_processed_tweets(&a, &b, parallel)
+                })
+                .unwrap(),
+            parallel,
+        )
+    } else {
+        processed_tweets_to_priority_queue(
+            tweets
+                .iter()
+                .map(|tweet: &String| process_tweet(tweet))
+                .reduce(|a: DashMap<String, i128>, b: DashMap<String, i128>| {
+                    combine_processed_tweets(&a, &b, parallel)
+                })
+                .unwrap(),
+            parallel,
+        )
+    }
 }
 
 fn process_tweet(tweet: &String) -> DashMap<String, i128> {
@@ -46,55 +60,99 @@ fn process_tweet(tweet: &String) -> DashMap<String, i128> {
     res
 }
 
-fn get_dashmap_keys(a: &DashMap<String, i128>) -> Vec<String> {
-    a.into_par_iter()
-        .map(|a: RefMulti<String, i128>| a.key().clone())
-        .collect()
+fn get_dashmap_keys(a: &DashMap<String, i128>, parallel: bool) -> Vec<String> {
+    if parallel {
+        a.into_par_iter()
+            .map(|a: RefMulti<String, i128>| a.key().clone())
+            .collect()
+    } else {
+        a.into_iter()
+            .map(|a: RefMulti<String, i128>| a.key().clone())
+            .collect()
+    }
 }
 
 fn combine_processed_tweets(
     a: &DashMap<String, i128>,
     b: &DashMap<String, i128>,
+    parallel: bool,
 ) -> DashMap<String, i128> {
-    let keys: Vec<String> = get_dashmap_keys(a)
+    let keys: Vec<String> = get_dashmap_keys(a, parallel)
         .into_iter()
-        .chain(get_dashmap_keys(b).into_iter())
+        .chain(get_dashmap_keys(b, parallel).into_iter())
         .collect();
     let res: DashMap<String, i128> = DashMap::new();
 
-    keys.into_par_iter().for_each(|key: String| {
-        let key_str = key.as_str();
+    if parallel {
+        keys.into_par_iter().for_each(|key: String| {
+            let key_str = key.as_str();
 
-        match b.get(key_str) {
-            Some(b_count) => match a.get(key_str) {
-                Some(a_count) => {
-                    res.insert(key, a_count.clone() + b_count.clone());
-                }
-                _ => {
-                    res.insert(key, b_count.clone());
-                }
-            },
-            _ => {
-                match a.get(key_str) {
-                    Some(val) => {
-                        res.insert(key, val.clone());
+            match b.get(key_str) {
+                Some(b_count) => match a.get(key_str) {
+                    Some(a_count) => {
+                        res.insert(key, a_count.clone() + b_count.clone());
                     }
-                    _ => {}
-                };
+                    _ => {
+                        res.insert(key, b_count.clone());
+                    }
+                },
+                _ => {
+                    match a.get(key_str) {
+                        Some(val) => {
+                            res.insert(key, val.clone());
+                        }
+                        _ => {}
+                    };
+                }
             }
-        }
-    });
+        });
+    } else {
+        keys.into_iter().for_each(|key: String| {
+            let key_str = key.as_str();
+
+            match b.get(key_str) {
+                Some(b_count) => match a.get(key_str) {
+                    Some(a_count) => {
+                        res.insert(key, a_count.clone() + b_count.clone());
+                    }
+                    _ => {
+                        res.insert(key, b_count.clone());
+                    }
+                },
+                _ => {
+                    match a.get(key_str) {
+                        Some(val) => {
+                            res.insert(key, val.clone());
+                        }
+                        _ => {}
+                    };
+                }
+            }
+        });
+    }
 
     res
 }
 
-fn processed_tweets_to_priority_queue(pt: DashMap<String, i128>) -> PriorityQueue<String, i128> {
-    let res: Mutex<PriorityQueue<String, i128>> = Mutex::new(PriorityQueue::new());
+fn processed_tweets_to_priority_queue(
+    pt: DashMap<String, i128>,
+    parallel: bool,
+) -> PriorityQueue<String, i128> {
+    if parallel {
+        let res: Mutex<PriorityQueue<String, i128>> = Mutex::new(PriorityQueue::new());
 
-    pt.into_par_iter().for_each(|tuple_val: (String, i128)| {
-        let mut q = res.lock().unwrap();
-        q.push(tuple_val.0, tuple_val.1);
-    });
+        pt.into_par_iter().for_each(|tuple_val: (String, i128)| {
+            let mut q = res.lock().unwrap();
+            q.push(tuple_val.0, tuple_val.1);
+        });
 
-    res.into_inner().unwrap()
+        res.into_inner().unwrap()
+    } else {
+        let mut q = PriorityQueue::new();
+        pt.into_iter().for_each(|tuple_val: (String, i128)| {
+            q.push(tuple_val.0, tuple_val.1);
+        });
+
+        q
+    }
 }
