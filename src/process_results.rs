41236ba4b7ffd078437b5d32451df32330419327
Tom Hardern <thomasaeh1@gmail.com>
2021-08-20 16:31:51 +0100

    added detail to the todo in process_results.rs

--- src/process_results.rs
+++ src/process_results.rs
@@ -1,5 +1,5 @@
 use crate::TweetProcessingResult;
 
 pub fn process_results(algorithm_results: Vec<TweetProcessingResult>) {
-    //TODO: write the results to a csv and make visualisations of them
+    //TODO: write the results to a csv (use polars), make stats measurements (use statrs), and make visualisations of them (use plotters)
 }
