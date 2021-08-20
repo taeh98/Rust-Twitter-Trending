438ae6ec63886e4119b7811d97af3901380129ab
Tom Hardern <thomasaeh1@gmail.com>
2021-08-20 20:41:57 +0100

    added and corrected stats and visualisations to make listed in process_results.rs

--- src/process_results.rs
+++ src/process_results.rs
@@ -9,15 +9,18 @@ pub fn process_results(algorithm_results: Vec<TweetProcessingResult>) {
     bar chart of mean time taken and tweets per second for each algorithm
     box plots of each value of times taken and tweets per second rates for each algorithm
     dot plots of each value of times taken and tweets per second rates for each algorithm
+    scatter plot of test number and time taken and test number and tweets/second for each algorithm
 
     STATS (indented are non-parametric alternatives to above parametric tests)
     mean, median, mode, std dev, variance, IQR of times taken and processing speeds for each algorithm
     independent samples t-tests between times taken and tweets per second rates of all algorithms
         Wilcoxon Rank-Sum tests between times taken and tweets per second rates of all algorithms
     one-way anova tests between times taken and tweets per second rates of all algorithms
-        Kruskal Wallis H Tess between times taken and tweets per second rates of all algorithms
+        Kruskal Wallis H Tests between times taken and tweets per second rates of all algorithms
     Chi-squared test to see how dependent the categorical variables (Rust or Python and serial or parallel) are
+    Phi coefficient to see how associated the categorical variables (Rust or Python and serial or parallel) are
     F-test to compare the variances of the samples
+    pearson and spearman correlation coefficients for test number vs time taken for all algorithms
 
     */
 }
