fe4d9b93c9d38bb2679c6aa6ec94446c77c70060
Tom Hardern <thomasaeh1@gmail.com>
2021-08-27 08:33:48 +0100

    added list of commands to download file to verbose_file_download.rs

--- src/get_data/verbose_file_download.rs
+++ src/get_data/verbose_file_download.rs
@@ -1,3 +1,7 @@
 pub fn download_file_with_progress(url_src: &str, filepath_dest: &str) {
     //TODO: download latest file with progress bar (like wget)
 }
+
+// docker run curlimages/curl:7.78.0 -L <url_src> -o <filepath_dest> --name curl-<filepath_dest>
+// docker wait curl-<filepath_dest>
+// docker rm curl-<filepath_dest>
