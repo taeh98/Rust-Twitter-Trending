6205f7521313b7013de4cb00458e242c975361b1
Tom Hardern <thomasaeh1@gmail.com>
2021-09-03 13:52:27 +0100

    moved mistaken logic for calling plot generation function from dot_plots.rs to scatter_plots.rs

--- src/process_results/make_visualisations/dot_plots.rs
+++ src/process_results/make_visualisations/dot_plots.rs
@@ -22,31 +22,29 @@ const DOT_PLOTS_OUTPUT_FILES_DIRECTORY: &'static str =
 
 pub(crate) fn make_dot_plots(
     algorithm_names: &Vec<String>,
-    time_taken_values_list: &Vec<Vec<f64>>,
-    processing_speed_values_list: &Vec<Vec<f64>>,
+    time_taken_values: &Vec<Vec<f64>>,
+    processing_speed_values: &Vec<Vec<f64>>,
 ) {
-    if !Path::new(DOT_PLOTS_OUTPUT_FILES_DIRECTORY).exists() {
-        create_dir(DOT_PLOTS_OUTPUT_FILES_DIRECTORY)
-            .expect("Couldn't create the out/visualisations/dot_plots/ directory.");
+    if !Path::new(BOX_PLOTS_OUTPUT_FILES_DIRECTORY).exists() {
+        create_dir(BOX_PLOTS_OUTPUT_FILES_DIRECTORY)
+            .expect("Couldn't create the out/visualisations/box_plots/ directory.");
     }
 
-    algorithm_names
-        .iter()
-        .zip(time_taken_values_list.iter())
-        .zip(processing_speed_values_list.iter())
-        .for_each(
-            |((algorithm_name, time_taken_values), processing_speed_values)| {
-                gen_dot_plot(algorithm_name, time_taken_values, Variable::TimeTaken);
-                gen_dot_plot(
-                    algorithm_name,
-                    processing_speed_values,
-                    Variable::ProcessingSpeed,
-                );
-            },
-        );
+    [
+        (Variable::TimeTaken, time_taken_values),
+        (Variable::ProcessingSpeed, processing_speed_values),
+    ]
+    .into_par_iter()
+    .for_each(|var_values_pair: (Variable, &Vec<Vec<f64>>)| {
+        gen_dot_plot(algorithm_names, var_values_pair.1, var_values_pair.0)
+    })
 }
 
-fn gen_dot_plot(algorithm_name: &String, values: &Vec<f64>, variable: Variable) {
+fn gen_dot_plot(
+    algorithm_names: &Vec<String>,
+    algorithm_values: &Vec<Vec<f64>>,
+    variable: Variable,
+) {
     // Define chart related sizes.
     let width: isize = CHART_WIDTH_PIXELS;
     let height: isize = CHART_HEIGHT_PIXELS;
--- src/process_results/make_visualisations/scatter_plots.rs
+++ src/process_results/make_visualisations/scatter_plots.rs
@@ -20,27 +20,28 @@ const SCATTER_PLOTS_OUTPUT_FILES_DIRECTORY: &'static str =
 
 pub(crate) fn make_scatter_plots(
     algorithm_names: &Vec<String>,
-    time_taken_values: &Vec<Vec<f64>>,
-    processing_speed_values: &Vec<Vec<f64>>,
+    time_taken_values_list: &Vec<Vec<f64>>,
+    processing_speed_values_list: &Vec<Vec<f64>>,
 ) {
-    if !Path::new(SCATTER_PLOTS_OUTPUT_FILES_DIRECTORY).exists() {
-        create_dir(SCATTER_PLOTS_OUTPUT_FILES_DIRECTORY)
-            .expect("Couldn't create the out/visualisations/scatter_plots/ directory.");
+    if !Path::new(DOT_PLOTS_OUTPUT_FILES_DIRECTORY).exists() {
+        create_dir(DOT_PLOTS_OUTPUT_FILES_DIRECTORY)
+            .expect("Couldn't create the out/visualisations/dot_plots/ directory.");
     }
 
-    [
-        (Variable::TimeTaken, time_taken_values),
-        (Variable::ProcessingSpeed, processing_speed_values),
-    ]
-    .into_par_iter()
-    .for_each(|var_values_pair: (Variable, &Vec<Vec<f64>>)| {
-        gen_scatter_plot(algorithm_names, var_values_pair.1, var_values_pair.0)
-    })
+    algorithm_names
+        .iter()
+        .zip(time_taken_values_list.iter())
+        .zip(processing_speed_values_list.iter())
+        .for_each(
+            |((algorithm_name, time_taken_values), processing_speed_values)| {
+                gen_scatter_plot(algorithm_name, time_taken_values, Variable::TimeTaken);
+                gen_dot_plot(
+                    algorithm_name,
+                    processing_speed_values,
+                    Variable::ProcessingSpeed,
+                );
+            },
+        );
 }
 
-fn gen_scatter_plot(
-    algorithm_names: &Vec<String>,
-    algorithm_values: &Vec<Vec<f64>>,
-    variable: Variable,
-) {
-}
+fn gen_scatter_plot(algorithm_name: &String, values: &Vec<f64>, variable: Variable) {}
