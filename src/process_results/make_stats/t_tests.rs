54f1297471ff52ea1ff311fefb3c6b88b86ee523
Tom Hardern <thomasaeh1@gmail.com>
2021-09-04 10:33:32 +0100

    added basic logic to t_tests.rs

--- src/process_results/make_stats/t_tests.rs
+++ src/process_results/make_stats/t_tests.rs
@@ -13,8 +13,10 @@ use mathru::{
         test::{Test, T},
     },
 };
+use rayon::iter::IntoParallelIterator;
 
 use crate::process_results::make_stats::STATS_OUTPUT_FILES_DIRECTORY;
+use crate::process_results::{Variable, ALL_VARIABLE_VALUES};
 
 const T_TESTS_OUTPUT_FILES_DIRECTORY: &'static str =
     concatcp!(STATS_OUTPUT_FILES_DIRECTORY, "/t_tests") as &'static str;
@@ -29,6 +31,49 @@ pub(crate) fn make_t_tests(
             .expect("Couldn't create the out/stats/t_tests/ directory.");
     }
 
+    let algorithm_names_values: Vec<(&String, (&Vec<f64>, &Vec<f64>))> = algorithm_names
+        .iter()
+        .zip(time_taken_values.iter().zip(processing_speed_values.iter()))
+        .collect();
+
+    ALL_VARIABLE_VALUES
+        .into_par_iter()
+        .for_each(|variable: Variable| {
+            algorithm_names_values.into_par_iter().for_each(
+                |(algorithm_name_a, (time_taken_values_a, processing_speed_values_a))| {
+                    algorithm_names_values.into_par_iter().for_each(
+                        |(algorithm_name_b, (time_taken_values_b, processing_speed_values_b))| {
+                            let values_a = match variable {
+                                Variable::ProcessingSpeed => processing_speed_values_a,
+                                _ => time_taken_values_a,
+                            };
+
+                            let values_b = match variable {
+                                Variable::ProcessingSpeed => processing_speed_values_b,
+                                _ => time_taken_values_b,
+                            };
+
+                            gen_t_test(
+                                algorithm_name_a,
+                                algorithm_name_b,
+                                values_a,
+                                values_b,
+                                &variable,
+                            );
+                        },
+                    )
+                },
+            );
+        });
+}
+
+fn gen_t_test(
+    algorithm_a_name: &String,
+    algorithm_b_name: &String,
+    values_a: &Vec<f64>,
+    values_b: &Vec<f64>,
+    variable: &Variable,
+) {
     let rv1: Vec<f64> = Normal::new(1.0, 0.5).random_sequence(100);
     let rv2: Vec<f64> = Normal::new(1.0, 0.5).random_sequence(100);
 
