a57bf991ce26e267f8c5e987bdfde55f0b8fe310
Tom Hardern <thomasaeh1@gmail.com>
2021-09-04 11:28:41 +0100

    grouped t test results by variable

--- src/process_results/make_stats/t_tests.rs
+++ src/process_results/make_stats/t_tests.rs
@@ -4,15 +4,14 @@
 
 use std::fs::create_dir;
 use std::path::Path;
+use std::sync::{Mutex, MutexGuard};
 
 use const_format::concatcp;
 use mathru::{
     self,
-    statistics::{
-        distrib::{Distribution, Normal},
-        test::{Test, T},
-    },
+    statistics::test::{Test, T},
 };
+use rayon::iter::IndexedParallelIterator;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 use crate::process_results::make_stats::STATS_OUTPUT_FILES_DIRECTORY;
@@ -31,49 +30,63 @@ pub(crate) fn make_t_tests(
             .expect("Couldn't create the out/stats/t_tests/ directory.");
     }
 
-    let algorithm_names_values: Vec<(&String, (&Vec<f64>, &Vec<f64>))> = algorithm_names
-        .iter()
-        .zip(time_taken_values.iter().zip(processing_speed_values.iter()))
-        .collect();
-
     ALL_VARIABLE_VALUES
         .into_par_iter()
         .for_each(|variable: Variable| {
-            (&algorithm_names_values).into_par_iter().for_each(
-                |(algorithm_name_a, (time_taken_values_a, processing_speed_values_a))| {
-                    (&algorithm_names_values).into_par_iter().for_each(
-                        |(algorithm_name_b, (time_taken_values_b, processing_speed_values_b))| {
-                            let values_a = match variable {
-                                Variable::ProcessingSpeed => processing_speed_values_a,
-                                _ => time_taken_values_a,
-                            };
-
-                            let values_b = match variable {
-                                Variable::ProcessingSpeed => processing_speed_values_b,
-                                _ => time_taken_values_b,
-                            };
-
-                            gen_t_test(
-                                algorithm_name_a,
-                                algorithm_name_b,
-                                values_a,
-                                values_b,
-                                &variable,
-                            );
-                        },
-                    )
-                },
-            );
+            let algorithm_names_variable_values: Vec<(&String, &Vec<f64>)> = algorithm_names
+                .iter()
+                .zip(
+                    (match variable {
+                        Variable::ProcessingSpeed => processing_speed_values,
+                        _ => time_taken_values,
+                    })
+                    .iter(),
+                )
+                .collect();
+            run_t_tests_for_variable(&variable, &algorithm_names_variable_values);
         });
 }
 
-fn gen_t_test(
-    algorithm_a_name: &String,
-    algorithm_b_name: &String,
-    values_a: &Vec<f64>,
-    values_b: &Vec<f64>,
+fn run_t_tests_for_variable(
     variable: &Variable,
+    algorithm_names_variable_values: &Vec<(&String, &Vec<f64>)>,
 ) {
-    let student_t_test: T<f64> = T::test_independence_equal_variance(values_a, values_b);
-    let welch_t_test: T<f64> = T::test_independence_unequal_variance(values_a, values_b);
+    let res_mutex: Mutex<Vec<(&String, &String, f64, f64)>> = Mutex::new(Vec::new());
+
+    let index_algorithm_names_variable_values: Vec<(usize, &(&String, &Vec<f64>))> =
+        algorithm_names_variable_values
+            .into_par_iter()
+            .enumerate()
+            .collect::<Vec<(usize, &(&String, &Vec<f64>))>>();
+
+    index_algorithm_names_variable_values
+        .into_par_iter()
+        .for_each(|(index_a, (algorithm_name_a, values_a))| {
+            let other_algorithm_names_variable_values: Vec<(&String, &Vec<f64>)> =
+                algorithm_names_variable_values
+                    .split_at(index_a + 1)
+                    .1
+                    .to_vec();
+
+            other_algorithm_names_variable_values
+                .into_par_iter()
+                .for_each(|(algorithm_name_b, values_b)| {
+                    let student_t_test: T<f64> =
+                        T::test_independence_equal_variance(values_a, values_b);
+                    let welch_t_test: T<f64> =
+                        T::test_independence_unequal_variance(values_a, values_b);
+
+                    let mut inner_results: MutexGuard<Vec<(&String, &String, f64, f64)>> =
+                        res_mutex.lock().unwrap();
+
+                    inner_results.push((
+                        algorithm_name_a,
+                        algorithm_name_b,
+                        student_t_test.p_value(),
+                        welch_t_test.p_value(),
+                    ));
+                });
+        });
+
+    let result: Vec<(&String, &String, f64, f64)> = res_mutex.into_inner().ok().unwrap();
 }
